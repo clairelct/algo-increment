7f8115434ef5aa30393c6e4b47bf6159a6f48b65
Claire Leconte <claire.lcnt@gmail.com>
2021-06-24 12:05:33 +0200

    Review increment()

--- custom-number-type-increment.js
+++ custom-number-type-increment.js
@@ -1,29 +1,45 @@
+// [Algo] Increment a custom Number type
+// Your mission is to write an increment function that adds 1 to the input Array<number> and returns the incremented Array<number>.
+
 const increment = (number) => {
   const newArray = [];
-  // For each index beginning by the last one...
+  let allNineCount = 0;
+
+  // Questionning each index beginning by the last one...
   for (let i = number.length - 1; i >= 0; i--) {
-    // If index !== 9
-    if (number[i] < 9) {
-      number[i]++;
-      return number;
-    } else {
-      // Index === 9, previous index !== 9
-      // Index => 0, previous index++
-      if (number[i - 1] !== 9) {
-        number[i] = 0;
-        number[i - 1]++;
-        return number;
+    
+      // 1. If index < 9, just increment it
+      if (number[i] < 9) {
+
+        number[i]++;
+        return number; 
+
       } else {
-        // Index === 9 && previous index === 9
-        // Index => 0, previous index => 0, previous index++
-        number[i] = 0;
-        number[i - 1] = 0;
-        number[i - 2]++;
-        // Maybe define a loop for previous indexes..
-        return number;
-      }
-    }
+          // Switch '9' to '0'
+          allNineCount++;
+          number[i] = 0;
+          newArray.push(number[i]);
+
+          // 2. If all indexes are '=== 9'
+          // Then push a new first '1' index at start of array
+          if (allNineCount === number.length){
+            newArray.unshift(1);
+          // 3. If all indexes are '=== 9' except [0]
+          // Then push a new last '0' index at the end of array
+          } else if (allNineCount === number.length - 1 && number[0] !== 9){
+            newArray.push(0);
+          }
+      }         
   }
+
+  if (newArray.length > 0) {return newArray}
+
 };
 
+
+console.log(increment([3, 2, 0, 8, 9]));
 console.log(increment([3, 2, 0, 9, 9]));
+console.log(increment([4, 9, 9, 9, 9]));
+console.log(increment([9, 5, 9]));
+console.log(increment([9, 9]));
+console.log(increment([1]));
